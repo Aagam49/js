7112b68b1847c0cde6a51312f0b9451dcace51de
Aagam shah <121475512+Aagam49@users.noreply.github.com>
2025-12-18 12:47:45 +0000

    add data types example in 02_dataTypes.js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict";
+// alert(3+3)
+// JavaScript Data Types
+
+// 1. Primitive Data Types
+let num = 42;                     // Number
+let str = "Hello, World!";       // String
+let bool = true;                 // Boolean
+let undef;                       // Undefined
+let nul = null;                  // Null
+let sym = Symbol("id");          // Symbol
+let bigInt = 9007199254740991n;  // BigInt
+
+console.log(typeof undefined);
+console.log(typeof null);
