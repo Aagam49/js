35997ef69a8c9f40240d3a44d23a110504f0d612
Aagam shah <121475512+Aagam49@users.noreply.github.com>
2025-12-19 11:33:23 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 123456
+let accountEmail = "aagam494@gmail.com"
+var accountPassword = "securepassword123"
+accountCity = "Ahmedabad"
+let accountState;
+
+// accountId = 654321
+// console.log(accountId);
+
+accountEmail = "aagam8392@gmail.com"
+accountPassword = "newsecurepassword456"
+accountCity = "New York"
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+console.log(accountEmail)
